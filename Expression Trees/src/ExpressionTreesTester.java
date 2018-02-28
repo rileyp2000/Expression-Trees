49d4b3076c6a01e6bf35fe43cb00db2a285f830c
rileyp2000 <rileyp@bishopireton.org>
2018-02-28 10:46:03 -0500

    testing

--- Expression Trees/src/ExpressionTreesTester.java
+++ Expression Trees/src/ExpressionTreesTester.java
@@ -5,6 +5,7 @@
  */
 public class ExpressionTreesTester {
 	public static void main(String[] args) {
-
+		//(3+4)*2+5*6
+		String[] expression = new String[] {"3","4","+","2","*","5","6","*","+"};
 	}
 }
