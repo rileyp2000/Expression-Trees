a0e81d0780037a606c1f428fe9740b871d6b7201
rileyp2000 <rileyp@bishopireton.org>
2018-03-06 10:42:33 -0500

    division no workie

--- Expression Trees/src/ExpressionTrees.java
+++ Expression Trees/src/ExpressionTrees.java
@@ -50,7 +50,9 @@ public class ExpressionTrees extends TreeNode implements Expressions {
 				int i = Integer.parseInt(o);
 				s.push(new ExpressionTrees(i));
 			} catch (NumberFormatException e) {
-				ExpressionTrees r = new ExpressionTrees(o, s.pop(), s.pop());
+				ExpressionTrees r = new ExpressionTrees(o);//,s.pop(),s.pop());
+				r.setRight(s.pop());
+				r.setLeft(s.pop());
 				s.push(r);
 			}
 		}
